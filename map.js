61231ceaaff37449b7b3c861bdc5166a8845ea3a
nobohan <julien_wa@yahoo.fr>
2016-08-09 09:27:04 +0200

    display panel in mobile view

--- map.js
+++ map.js
@@ -4,35 +4,24 @@
 var scaleLine = new ol.control.ScaleLine()
 
 // Map creation
-var map = new ol.Map({
+var olmap = new ol.Map({
    target: document.getElementById('map'),  // instead of "target: 'map' " because of the cursor pointer
    view: new ol.View({
-   	center: ol.proj.transform([4.5, 50.5], 'EPSG:4326', 'EPSG:3857'),
+   	center: ol.proj.transform([3.65, 50.5], 'EPSG:4326', 'EPSG:3857'),
       zoom: 8
    }),
    controls: ol.control.defaults({scaleLine: false}).extend([scaleLine]),
 });
 
-// Add layers
-
-var setLayer = function(layername){
-	layer = new ol.layer.Tile({
-	source: new ol.source.TileWMS({
-	    url: 'http://localhost/cgi-bin/mapserv?map=/var/www/CGMS/mapserver/ol3/meteo_tx_shp.map&request=GetMap&service=WMS&map.layer[0]=data+'+layername+'.shp',
-	    params: {LAYERS: 'meteo'} 
-	    })
-	})
-	map.addLayer(layer);
-};
-
-setLayer('met');
-
+// Add default layers
+var osmLayer = new ol.layer.Tile({
+   source: new ol.source.OSM()
+});
+olmap.addLayer(osmLayer);
 
 // Add specific classes to OpenLayers elements
-
 $('.ol-scale-line').addClass('hidden-xs')
 $('.ol-attribution').addClass('hidden-xs')
-
 
 // Hide/show panel
 var showPanel = true;
@@ -48,6 +37,21 @@ var collapsePanel = function(){
 	  $('div#panelContent').css('opacity','1');
 	  $('div#collapseBtn button').text('<');
 	  showPanel =! showPanel;
+	  }
+}
+
+var showPanelXs = false;
+var collapsePanelXs = function(){
+	if(showPanelXs === true){
+	  $('div#panel').css('width','0px');
+	  $('div#panelContent').css('opacity','0' );
+	  showPanelXs =! showPanelXs;
+	  }
+   else{
+     $('div#panel').css('width','calc(100% - 45px)');
+     $('div#panelContent').css('opacity','1');
+     $('div#navbar').removeClass('in')
+	  showPanelXs =! showPanelXs;
 	  }
 }
 
