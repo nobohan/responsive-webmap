8480253f7c25a3dcd9e51f39cf5348e20aacb161
nobohan <julien_wa@yahoo.fr>
2016-08-30 21:39:25 +0200

    Add table in modal window using D3 functions

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 // OpenLayers 3 - Test with mapserver for B-CGMS - Julien Minet - July 2016. 
 
-// Forms functions
+
+// 1) Forms - data functions
 
 // Initialize the date pickers
 $(".formDate").datepicker({
@@ -8,41 +9,150 @@ $(".formDate").datepicker({
     	defaultDate: "2013-06-08"
 });  
 
-// updateLayer button
-var updateLayer = function(){
+// Get user inputs from the #generalForm
+var getUserInputs = function () {
    var variable = $('#formVariable').val();
    var stat = $('#formStat').val();
    var spatial = $('#formSpatial').val();
-   //var date = $('#formDate').val();
    var date = $('#formDate').datepicker('getDate');
-   // if 'xs' do this:
-   // var date = $('#formDateXs').datepicker('getDate');
    var dd = (date.getDate() < 10 ? '0' : '') + date.getDate();
    var mm = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);   
    var yyyy = date.getFullYear();
-   console.log(variable, stat, spatial, yyyy, mm, dd);
-   // update map
-   // TODO: remove previous layers
-   // TODO: check if the layer exist!
-   //olmap.removeLayer()
-   showLayer('geom from meteo'+yyyy+mm+dd+'_grid', 'postgis');
+   //console.log(variable, stat, spatial, yyyy, mm, dd);
+   // TO DO: messages when fields are empty
+   return { 
+       variable: variable,
+       stat: stat,
+       spatial: spatial,
+       yyyy: yyyy, 
+       mm: mm,
+       dd: dd
+   }
+}
+
+
+// getData function (if needed?)
+// TODO: select data from database, with a SQL command, instead of loading csv with D3
+
+
+// updateLayer button - show layer
+var updateLayer = function(){
+   var inputs = getUserInputs();
+   // TODO: remove previous layers olmap.removeLayer()
+   
+   showLayer('geom from meteo'+inputs.yyyy+inputs.mm+inputs.dd+'_grid', 'postgis');
 
    // TODO: show legend   
 
 }
 
-// UpdateData button
-var updateData = function(){
-	var variable = $('#formVariable').val();
-   var stat = $('#formStat').val();
-   var spatial = $('#formSpatial').val();
-   var date = $('#formDate').datepicker('getDate')
-   var dd = (date.getDate() < 10 ? '0' : '') + date.getDate();
-   var mm = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);   
-   var yyyy = date.getFullYear();
-	// select Data with SQL command
-	// Update the modal content
+// showData button - show table
+// could probably be done in jquery
+var showData = function(){
+   dataset=[];	
+  
+   // Get user inputs
+   var inputs = getUserInputs();
+   
+   // Get data using user inputs
+	d3.csv('tables/meteo'+inputs.yyyy+inputs.mm+inputs.dd+'_table.csv', function(csvdata) {
+	   dataset = csvdata.map(function(d) { return [ +d["GRID_NO"], +d["TX"] ]; })  
+      // Get table header
+      dataheader = ['GRID', inputs.variable];	   // TO DO: load it from csv (or not)
+	    
+	   // Update the modal content
+	   // Add modal title:
+	   $('.modal-title').replaceWith(inputs.variable + ' data for ' + inputs.yyyy + "/" + inputs.mm + "/" + inputs.dd);
+	    
+	   // Select table 
+	   var table = d3.select("#modal-table"),
+	   thead = table.append("thead"),
+	   tbody = table.append("tbody");
+	
+	   // append the header
+	   thead.append("tr")
+	        .selectAll("th")
+	        .data(dataheader)
+	        .enter()
+	        .append("th")
+	            .text(function(column) { return column; });
+	            
+	   // append the data
+	   table.selectAll("tr").data(dataset).enter().append("tr")
+           .selectAll("td")
+              .data(function(d) { return d; }).enter()
+              .append("td")
+              .text(function(d) { return d; });
+ 	    
+	})
 }
+
+// show graph
+var showGraph = function () {
+	
+   // Get user inputs
+   var inputs = getUserInputs();
+   
+   // Get data using user inputs   
+   d3.csv('tables/meteo'+inputs.yyyy+inputs.mm+inputs.dd+'_table.csv', function(csvdata) {
+	   dataset = csvdata.map(function(d) { return [ +d["TX"] ]; })  
+      
+      // Build graph
+
+		var formatCount = d3.format(",.0f");
+		
+		var margin = {top: 10, right: 30, bottom: 30, left: 30},
+		    width = 960 - margin.left - margin.right,
+		    height = 500 - margin.top - margin.bottom;
+		
+		var x = d3.scaleLinear()
+		    .domain([0, 50])
+		    .rangeRound([0, width]);
+		
+		var bins = d3.histogram()
+		    .domain(x.domain())
+		    .thresholds(x.ticks(20))
+		    (dataset);
+		
+		var y = d3.scaleLinear()
+		    .domain([0, d3.max(bins, function(d) { return d.length; })])
+		    .range([height, 0]);	    
+
+		var hist = d3.select("#modal-graph").append("svg")
+		    .attr("width", width + margin.left + margin.right)
+		    .attr("height", height + margin.top + margin.bottom)
+		  .append("g")
+		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		
+		var bar = hist.selectAll(".bar")
+		    .data(bins)
+		  .enter().append("g")
+		    .attr("class", "bar")
+		    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+		
+		bar.append("rect")
+		    .attr("x", 1)
+		    .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+		    .attr("height", function(d) { return height - y(d.length); });
+		
+		bar.append("text")
+		    .attr("dy", ".75em")
+		    .attr("y", 6)
+		    .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+		    .attr("text-anchor", "middle")
+		    .text(function(d) { return formatCount(d.length); });
+		
+		hist.append("g")
+		    .attr("class", "axis axis--x")
+		    .attr("transform", "translate(0," + height + ")")
+		    .call(d3.axisBottom(x));  
+   })
+}
+	
+
+
+
+// 2) Map - OpenLayers functions
 
 // Definition of OpenLayers controls
 var scaleLine = new ol.control.ScaleLine()
@@ -63,25 +173,16 @@ var osmLayer = new ol.layer.Tile({
 });
 olmap.addLayer(osmLayer);
 
-
-// Add layers functions
-
-// Progress when the source is loading
+// Progress cursor when the source is loading
 var showProgress = function (source) {
    source.on('tileloadstart', function() {
       $('.mapContainer').addClass('loading');
        });
    source.on('tileloadend', function() {
-      $('.mapContainer').removeClass('loading');
-      //$('div#map').removeClass('loading');
-      //$('div#panel').removeClass('loading'); 
-      //$('div#truc').removeClass('loading');   
+      $('.mapContainer').removeClass('loading');  
 	   });
 	source.on('tileloaderror', function() {
-      $('.mapContainer').removeClass('loading');  
-      //$('div#map').removeClass('loading');
-      //$('div#panel').removeClass('loading'); 
-      //$('div#truc').removeClass('loading');                  
+      $('.mapContainer').removeClass('loading');                   
 	   });
 };
 
@@ -104,22 +205,8 @@ var showLayer = function(layername, format){
 	 
 };
 
-var showLayerPostGISsimple = function(layername){
-	layer = new ol.layer.Tile({
-	source: new ol.source.TileWMS({
-	    url: 'http://localhost/cgi-bin/mapserv?map=/var/www/CGMS/proto/meteo_tx_postgis_'+layername+'.map',
-	    params: {LAYERS: 'meteo'} 
-	    })
-	})
-	olmap.addLayer(layer);
-};
 
-//showLayer('met.shp', 'shp');
-//showLayer('geom from meteo20130608_grid', 'postgis')
-
-//setLayerPostGISsimple('07')
-//setLayerPostGISsimple('08')
-
+// 3) User experience / responsiveness functions
 // Add specific classes to OpenLayers elements
 $('.ol-scale-line').addClass('hidden-xs')
 $('.ol-attribution').addClass('hidden-xs')
