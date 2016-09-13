2071fab9c649ec18c11a0f9d2f795acad6fda83a
nobohan <julien_wa@yahoo.fr>
2016-09-13 15:54:17 +0200

    Add comments to the js code

--- map.js
+++ map.js
@@ -7,10 +7,10 @@ var scaleLine = new ol.control.ScaleLine()
 var olmap = new ol.Map({
    target: document.getElementById('map'),  // instead of "target: 'map' " because of the cursor pointer
    view: new ol.View({
-   	center: ol.proj.transform([3.65, 50.5], 'EPSG:4326', 'EPSG:3857'),
+   	center: ol.proj.transform([3.65, 50.5], 'EPSG:4326', 'EPSG:3857'),  // set the center of the map. By default, the map projection is EPSG:3857
       zoom: 8
    }),
-   controls: ol.control.defaults({scaleLine: false}).extend([scaleLine]),
+   controls: ol.control.defaults({scaleLine: false}).extend([scaleLine]),  // Add the scaleline control
 });
 
 // Add default layers
@@ -19,11 +19,11 @@ var osmLayer = new ol.layer.Tile({
 });
 olmap.addLayer(osmLayer);
 
-// Add specific classes to OpenLayers elements
+// Add specific classes to OpenLayers elements: hide these controls for mobile view using Bootstrap classes
 $('.ol-scale-line').addClass('hidden-xs')
 $('.ol-attribution').addClass('hidden-xs')
 
-// Hide/show panel
+// Hide/show panel function for desktop view. The panel is shown by default. 
 var showPanel = true;
 var collapsePanel = function(){
 	if(showPanel === true){
@@ -40,6 +40,7 @@ var collapsePanel = function(){
 	  }
 }
 
+// Hide/show panel function for mobile view. The panel is not shown by default.
 var showPanelXs = false;
 var collapsePanelXs = function(){
 	if(showPanelXs === true){
